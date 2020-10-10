f399c842e84c04b9d26a61a7711742b899c35b62
Alberto <edhoru@hotmail.com>
2020-10-09 21:46:21 -0500

    Initial Commit

---
+++ RIBsChildBecomesParent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RIBsChildBecomesParent
+//
+//  Created by Alberto Huerdo on 10/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
